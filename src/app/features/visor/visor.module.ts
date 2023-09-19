50aeb5fe19a6718b1f1e289b3ecef69cf5e75c61
hugo.herrador <hugo.herrador@inetum.com>
2023-09-19 12:24:16 +0200

    preliminary modification adaptations

--- src/app/features/visor/visor.module.ts
+++ src/app/features/visor/visor.module.ts
@@ -5,15 +5,19 @@ import { VisorRoutingModule } from './visor-routing.module';
 import { VisorComponent } from './visor.component';
 import { MapModule } from '@shared/components';
 import { VisorHeaderComponent } from './visor-header/visor-header.component';
+import { VisorFooterComponent } from './visor-footer/visor-footer.component';
+import { PrimengModule } from '@shared/components/primeng/primeng.module';
 
 @NgModule({
   declarations: [
     VisorComponent,
-    VisorHeaderComponent
+    VisorHeaderComponent,
+    VisorFooterComponent
   ],
   imports: [
     CommonModule,
     VisorRoutingModule,
+    PrimengModule,
     MapModule
   ]
 })
